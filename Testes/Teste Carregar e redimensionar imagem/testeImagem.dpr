deb3e43e0ffeb1802e669d99170cbf2297cdd412
DESKTOP-S4BD7LK\Ultrabook Lenovo <daniel_thls@yahoo.com.br>
2023-03-05 15:12:05 -0300

    Cria TServiceImage para lidar com manipulação de imagens no programa

---
+++ Testes/Teste Carregar e redimensionar imagem/testeImagem.dpr
@@ -0,0 +1,14 @@
+program testeImagem;
+
+uses
+  Vcl.Forms,
+  TesteImagem in 'TesteImagem.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
